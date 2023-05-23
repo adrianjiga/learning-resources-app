5713466430f217d96ed73dbd08b69bd636f31f94
Adrian Jiga <jiga.ion.adrian@gmail.com>
2023-05-23 14:10:49 +0300

    add BaseButton component and use it

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,39 @@
+<template>
+	<button :type="type" :class="mode">
+		<slot></slot>
+	</button>
+</template>
+
+<script>
+	export default {
+		props: ["type", "mode"],
+	};
+</script>
+
+<style scoped>
+	button {
+		padding: 0.75rem 1.5rem;
+		font-family: inherit;
+		background-color: #006434;
+		border: 1px solid #006434;
+		color: white;
+		cursor: pointer;
+	}
+
+	button:hover,
+	button:active {
+		background-color: #004119;
+		border-color: #004119;
+	}
+
+	.flat {
+		background-color: transparent;
+		color: #004119;
+		border: none;
+	}
+
+	.flat:hover,
+	.flat:active {
+		background-color: #bbffacc5;
+	}
+</style>
