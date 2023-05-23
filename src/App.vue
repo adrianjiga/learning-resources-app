9462ae6fd5dbfe2ef577f9a9af079d500204d590
Adrian Jiga <jiga.ion.adrian@gmail.com>
2023-05-23 10:37:01 +0300

    initialize learning resources app

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<ul v-for="storedResource in storedResources" :key="storedResource">
+		<li>
+			<h2>{{ storedResource.title }}</h2>
+			<h3>{{ storedResource.description }}</h3>
+			<h3>{{ storedResource.link }}</h3>
+		</li>
+	</ul>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				storedResources: [
+					{
+						id: "official-guide",
+						title: "Official Guide",
+						description: "The official Vue.js documentation",
+						link: "https://vuejs.org",
+					},
+					{
+						id: "google",
+						title: "Google",
+						description: "Learn to google...",
+						link: "https://google.com",
+					},
+				],
+			};
+		},
+	};
+</script>
